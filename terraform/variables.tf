f6596241dfc4f10cfb2cbe9128bf65a6baaf3f35
kishore-1906 <kishoresri75803@gmail.com>
2025-12-13 10:55:25 +0530

    Cd D

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,27 +6,33 @@ variable "region" {
 variable "ami_id" {
   type        = string
   description = "AMI for EC2 instance"
+  default     = ""   # safe for destroy
 }
 
 variable "subnet_id" {
-  type = string
+  type    = string
+  default = ""
 }
 
 variable "vpc_id" {
-  type = string
+  type    = string
+  default = ""
 }
 
 variable "key_name" {
-  type = string
+  type    = string
+  default = ""
 }
 
 variable "dockerhub_username" {
   type        = string
-  description = "kishore190"
+  description = "DockerHub username"
+  default     = "kishore190"
 }
 
 variable "image_tag" {
   type        = string
   description = "CI/CD generated image tag"
+  default     = "latest"
 }
 
